6f52ab27b54130de800da6bd57bed4c07ad4cfe2
William Budoyo <williambu07@gmail.com>
2018-06-16 22:48:49 -0400

    hw5

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,9 +2,25 @@
 // // Variables
 var startGame;
 // arrays that hold questions, answer option and the correct answer
-var questionsArray = ["What sports Michael Jordan played ?"];
-var answersOptionArray = ["Baseball Player ","BasketBall Player","Soccer Player","Chef"];
-var correctAnswer = ["BasketBall Player"];
+var questionsAndAnswer =[
+    {
+        questions: "Who Won 2018 SuperBowl ? ",
+        answerOptions: ["CowBoys" , "Patriot", "Eagles", "Brown"],
+        CorrectAnswer : "Eagles",
+        gif: "assets/gif/eagles.gif",
+
+        questions: "Who Won 2018 Nba Final ? ",
+        answerOptions: ["Cavalier" , "Sixers", "Lakers", "Warrior"],
+        CorrectAnswer : "Warrior",
+        gif: "assets/gif/warrior.gif",
+
+    }
+];
+
+//counter
+var winCounter=0;
+var lossCounter=0;
+var timerCountDown= 30;
 
 $(document).ready(function() {
 
@@ -13,50 +29,62 @@ $(document).ready(function() {
 //==========================================================================================================
 //Function
 
-    // function  that will create a start button to start
-    function initializeStart(){
-        
-        var startBtn= $("<button id= 'start-btn'>start</button>");
-        // startBtn = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
-        
-
-        $("#mainArea").html(startBtn);
-    }
 
  // function that create render the question and the options answers
-    function initilizeTriviaGame(){
+    function initilizeQuestions(){
+        timer = setInterval(timerCountDown,1000);
+        var triviaQuestions = questionsAndAnswer[0].questions;
 
-        var triviaQuestions = "<div id=questionsID>"+questionsArray[0];
-        $("#mainArea").html(triviaQuestions);
-        var triviaAnswer = "<div class =answerClass>";
-        for(i=0; i<answersOptionArray.length; i++){
+        $(".questions").html(triviaQuestions);
+        // var triviaAnswer = "<div class =answerClass>";
+        // for(i=0; i<answersOptionArray.length; i++){
             
-            $("#questionsID").append(triviaAnswer + answersOptionArray[i]);
-        }
+        //     $("#questionsID").append(triviaAnswer + answersOptionArray[i]);
+        // }
         
         
         console.log("test");
     }
+
+    //function for timer that count down 30 sec
+
+    function timer(){
+        // decrement the 30 seconds
+        timerCountDown--;
+
+        $(".timer").html(timerCountDown);
+    }// end timer()
     
 //==========================================================================================================
 //Main Process
-initializeStart();
+
 
 
 
 $("body").on("click", "#start-btn", function(event){
 
     console.log("you click me");
-
+    console.log()
+    // remove the start button
+    $("#start-btn").remove();
     // call the  function question and answer
-    initilizeTriviaGame();
+    timer()
+    initilizeQuestions();
 
 })// end click event start button;
 
 // click event when you click answer options 
 $("body").on("click", ".answerClass", function(event){
+    var selectedAnswer =$(this).text();
+    console.log(selectedAnswer);
 
-    console.log(this);
+    if(selectedAnswer === "Chef"){
+        alert("you win");
+
+    }else{
+        alert("you lose");
+    }
+    
 
    
 
